5cdb6d1f908545c245fc4076ba1758f352ae053e
BjMSCoder <122461578+BjMSCoder@users.noreply.github.com>
2023-01-11 15:28:12 +0100

    Added longitude of cercle and program to get hours, minutes and seconds from a number of seconds

--- Tasca3Programacio/src/main/java/com/mycompany/tasca3programacio/Tasca3Programacio.java
+++ Tasca3Programacio/src/main/java/com/mycompany/tasca3programacio/Tasca3Programacio.java
@@ -16,6 +16,7 @@ public class Tasca3Programacio {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
        
+        //Primer programa: Un fruiter necessita calcular els beneficis anuals que obté de la venda de pomes i peres. Per aquest motiu, és necessari dissenyar una aplicació que demani les vendes (en quilos) de cada trimestre per cada fruita.
         System.out.println("Introdueix els quilos de pomes del primer trimestre:");
         int kgPomesPrimerTrimestre = sc.nextInt();
         
@@ -49,12 +50,26 @@ public class Tasca3Programacio {
         double totalIngresos = totalIngresosPomes + totalIngresosPeres;
         System.out.println("El total d'ingresos de pomes i peres és: " + totalIngresos);
         
+        //Programa 2: Dissenyar una aplicació que calculi la longitud i l’àrea d’una circumferència. Per fer-ho, l’usuari ha d’introduir la longitud del radi.
         System.out.println("Introdueix la longitud en cm del radi:");
         double radi = sc.nextDouble();
         
         double area = Math.PI*Math.pow(radi, 2);
+                
+        double longitud = 2*Math.PI*radi;
         
-        System.out.println("L'àrea del cercle de radi:" + radi + " és: " + area);
+        System.out.println("L'àrea del cercle de radi: " + radi + " és: " + area + ". I la seva longitud és: " + longitud);
         
+        //Programa 3: Dissenyar una aplicació que sol·liciti a l’usuari una quantitat de segons. L’aplicació ha de mostrar quantes hores, minuts i segons hi ha en el nombre de segons introduïts per l’usuari.
+        
+        System.out.println("Introdueix una quantitat de segons:");
+        
+        int segonsIntroduits = sc.nextInt();
+        int hores = segonsIntroduits / 3600;
+        int minuts = (segonsIntroduits % 3600) / 60;
+        int segons = (segonsIntroduits % 3600) % 60;
+        
+        System.out.println("Hores: " + hores + ". \n" + ". Minuts: " + minuts + ". \n" + "Segons: " + segons + ".");
+
     }
 }
